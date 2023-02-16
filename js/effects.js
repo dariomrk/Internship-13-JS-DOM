cd22f2e52bf13762f1fd0d675a527ad1815dcab9
Dario <dariomrkonjic1@gmail.com>
2023-02-16 21:55:35 +0100

    Added Hover, Focus and Validation effects.

--- js/effects.js
+++ js/effects.js
@@ -1,19 +1,62 @@
 /**
- * @param {HTMLElement} target target HTML element
+ * Adds/removes a css class from a array of HTML elements when a addOn/removeOn event occurs on the eventTarget
+ * @param {keyof HTMLElementTagNameMap} eventTarget adds the event listeners to this element, this element is a target by default
  * @param {string} className css class name
  * @param {string} addOn the css class is added on this type of event
  * @param {string} removeOn the css class is removed on this type of event
+ * @param {Array<keyof HTMLElementTagNameMap>} targets
  */
-const addTiedEventListeners = (target, className, addOn, removeOn) => {
-  target.addEventListener(addOn, (e) => {
-    target.classList.add(className);
+const addTiedEventListeners = (
+  eventTarget,
+  className,
+  addOn,
+  removeOn,
+  targets = [eventTarget]
+) => {
+  const _eventTarget = document.querySelector(eventTarget);
+
+  _eventTarget.addEventListener(addOn, (e) => {
+    targets.forEach((t) => {
+      document.querySelector(t).classList.add(className);
+    });
   });
 
-  target.addEventListener(removeOn, (e) => {
-    target.classList.remove(className);
+  _eventTarget.addEventListener(removeOn, (e) => {
+    targets.forEach((t) => {
+      document.querySelector(t).classList.remove(className);
+    });
   });
 };
 
-export const addHoverEffect = (target) => {};
+/**
+ * @param {keyof HTMLElementTagNameMap} eventTarget adds the event listener to this element
+ * @param {(value: any) => boolean} validationPredicate used to validate the element data and determine whether to display the error
+ */
+export const addValidationEffect = (eventTarget, validationPredicate) => {
+  const _eventTarget = document.querySelector(eventTarget);
 
-export const addFocusEffect = (target) => {};
+  _eventTarget.addEventListener('blur', (e) => {
+    if (!validationPredicate(e.currentTarget.value)) {
+      _eventTarget.classList.add('error');
+
+      if (!_eventTarget.nextSibling.tagName)
+        _eventTarget.insertAdjacentHTML(
+          'afterend',
+          '<p class="form-input__error-message">This field is required</p>'
+        );
+      return;
+    }
+
+    if (_eventTarget.nextSibling.tagName)
+      _eventTarget.nextElementSibling.remove();
+    _eventTarget.classList.remove('error');
+  });
+};
+
+export const addHoverEffect = (eventTarget) => {
+  addTiedEventListeners(eventTarget, 'hover', 'mouseover', 'mouseleave');
+};
+
+export const addFocusEffect = (eventTarget, targets) => {
+  addTiedEventListeners(eventTarget, 'focus', 'focus', 'blur', targets);
+};
--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-const firstNameInput = document.querySelector('#user-first-name');
-const lastNameInput = document.querySelector('#user-last-name');
-const teachLevelSelect = document.querySelector('#teach-level');
-const teachDurationSelect = document.querySelector('#teach-duration');
+import {
+  addFocusEffect,
+  addHoverEffect,
+  addValidationEffect,
+} from './effects.js';
+
+addHoverEffect('#user-first-name');
+addFocusEffect('#user-first-name', ['#user-first-name', '#first-name-label']);
+addValidationEffect('#user-first-name', (value) => {
+  return value.trim() !== '';
+});
+
+addHoverEffect('#user-last-name');
+addFocusEffect('#user-last-name', ['#user-last-name', '#last-name-label']);
+addValidationEffect('#user-last-name', (value) => {
+  return value.trim() !== '';
+});
+
+addHoverEffect('#teach-level');
+addFocusEffect('#teach-level', ['#teach-level', '#teach-level-label']);
+
+addHoverEffect('#teach-duration');
+addFocusEffect('#teach-duration', ['#teach-duration', '#teach-duration-label']);
+
 const submitButton = document.querySelector('#submit');
