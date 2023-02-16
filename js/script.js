a743192ffba6c69c0c6bef0048eb64aed53e8050
Dario <dariomrkonjic1@gmail.com>
2023-02-16 18:34:16 +0100

    Added base HTML and CSS.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import { addEffects } from "./effects.js"
+
+const firstNameInput = document.querySelector('#user-first-name')
+const lastNameInput = document.querySelector('#user-last-name')
+const teachLevelSelect = document.querySelector('#teach-level')
+const teachDurationSelect = document.querySelector('#teach-duration')
+const submitButton = document.querySelector('#submit')
