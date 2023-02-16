3c1e112d4e716200f1940d84aae58c55dfac280d
Dario <dariomrkonjic1@gmail.com>
2023-02-16 18:40:37 +0100

    Removed unused import in script.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-import { addEffects } from "./effects.js"
-
 const firstNameInput = document.querySelector('#user-first-name')
 const lastNameInput = document.querySelector('#user-last-name')
 const teachLevelSelect = document.querySelector('#teach-level')
